5d095249994ce4683115c327a26e365ea84196af
alan.r <reale.ag@gmail.com>
2023-01-16 18:53:00 -0300

    mejorando componentes

--- my-react-app/src/components/formulario/Form.jsx
+++ my-react-app/src/components/formulario/Form.jsx
@@ -7,11 +7,11 @@ const Form = () => {
             <h4>Completá el formulario y estaremos en contacto.</h4>
             <form>
                 <label for="fname">Nombre:</label>
-                <input type="text" id="Name" name="fname" />
+                <input type="text" id="Name" name="fname" placeholder="Nombre" />
                 <label for="lname">Apellido:</label>
-                <input type="text" id="lastname" name="lname" />
+                <input type="text" id="lastname" name="lname" placeholder="Apellido" />
                 <label type="text">Comentarios</label>
-                <input type="text" id="comentarios"/>
+                <input type="text" id="comentarios" placeholder="Aquí podrás contarme tu propuesta"/>
                 <input type="submit" value="Submit"></input>
             </form>
         </div>
